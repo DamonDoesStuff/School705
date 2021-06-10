880d3b937ea79fd0e861c96635c2aac6c9257ab3
DamonRochon <83025646+DamonDoesStuff@users.noreply.github.com>
2021-06-10 15:56:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let day = "Sunday"
+let age = "12"
+let time = "12"
+
+if ((age < 18) && (day === "Monday" || day === "Tuesday" || day === "Wednsday" || day === "Thursday" || day == "Friday"))
+{
+  document.getElementById('answer').innerHTML = "Go to School"
+}
